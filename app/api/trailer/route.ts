65e7a6d70f8719bc2ca58922bac3cac37f0f1318
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-02-27 16:30:01 +0100

    Remove unused trailer API route and update package dependencies

--- app/api/trailer/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse, NextRequest } from "next/server";
-
-export async function GET(request: NextRequest) {
-    const searchParams = request.nextUrl.searchParams;
-    console.log(searchParams)
-    return NextResponse.json({ message: searchParams });
-}
-
