1ce20e862e3eedb284159ef008cfb6c3ada65f96
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-03-08 05:17:22 +0100

    Adjust padding in AdBlockBanner for improved visibility and user experience

--- components/layout/AdBlockBanner.tsx
+++ components/layout/AdBlockBanner.tsx
@@ -16,7 +16,7 @@ const AdBlockBanner = () => {
 
   return (
     isVisible && (
-      <div className="bg-gradient-to-r from-red-800 to-red-600 text-white py-0.5 text-xs px-2 fixed w-full top-0 z-[100] flex items-center justify-center gap-2">
+      <div className="bg-gradient-to-r from-red-800 to-red-600 text-white py-2 lg:py-0.5 text-xs px-2 fixed w-full top-0 z-[100] flex items-center justify-center gap-2">
         <AlertCircle className="w-4 h-4" />
         <span className="text-xs">For the best experience, please install</span>
         <Link
