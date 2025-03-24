81117862f14e67dac6929e4f21b7456df4f223be
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-03-24 14:06:55 +0100

    Update AdBlockBanner component styling and visibility logic; add Brave Browser link

--- components/layout/AdBlockBanner.tsx
+++ components/layout/AdBlockBanner.tsx
@@ -6,31 +6,39 @@ import { useEffect, useState } from "react";
 
 // Create a dismissible banner component
 const AdBlockBanner = () => {
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
     // Check if user has already dismissed the banner
-    setIsVisible(true)
     const dismissed = localStorage.getItem("adblock-banner-dismissed");
     if (dismissed) setIsVisible(false);
   }, []);
 
   return (
     isVisible && (
-      <div className="bg-gradient-to-r from-red-800 to-red-600 text-white py-2 lg:py-0.5 text-xs px-2 fixed w-full top-0 z-[100] flex items-center justify-center gap-2">
+      <div className="bg-amber-400 text-black py-2 lg:py-1.5 text-xs px-2 fixed w-full top-0 z-[100] flex items-center justify-center gap-2">
         <AlertCircle className="w-4 h-4" />
         <span className="text-xs">For the best experience, please install</span>
         <Link
           href="https://getadblock.com/en/"
           target="_blank"
           rel="noopener noreferrer"
-
-          className="font-medium underline hover:text-white/90 transition-colors"
+          className="font-medium underline hover:text-slate-900 transition-colors"
         >
           AdBlock
         </Link>
+        <span className="text-xs">or use</span>
+        <Link
+          href="https://brave.com/download/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-medium underline hover:text-slate-900 transition-colors"
+        >
+          Brave Browser
+        </Link>
+
         <X
-          className="w-4 h-4"
+          className="w-4 h-4  hover:text-slate-900 cursor-pointer"
           onClick={() => {
             setIsVisible(false);
             localStorage.setItem("adblock-banner-dismissed", "true");
